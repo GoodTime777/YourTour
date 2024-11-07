15a8feea47838156264bf7a9f8449adb27914560
Ольга <ots231088@gmail.com>
2024-11-07 19:27:49 +0300

    edits

--- indexHeader.js
+++ indexHeader.js
@@ -1,20 +1,20 @@
 const nav = document.querySelector(".header__navigation");
 const navFixed = document.querySelector(".navigation-scroll");
-const screenWidth = window.screen.width;
+//const screenWidth = window.screen.width;
 
-window.addEventListener("resize", () => {
-  if (screenWidth >= 360 && screenWidth < 1024) {
-    navFixed.style.top = `-88px`;
-  }
-});
+// window.addEventListener("resize", () => {
+//   if (screenWidth >= 360 && screenWidth < 1024) {
+//     navFixed.style.top = `-88px`;
+//   }
+// });
 
-window.addEventListener("scroll", () => {
-  const scrollValue = window.pageYOffset;
-  const navHeight = nav.clientHeight;
+// window.addEventListener("scroll", () => {
+//   const scrollValue = window.pageYOffset;
+//   const navHeight = nav.clientHeight;
 
-  if (scrollValue > 450) {
-    navFixed.style.top = `0px`;
-  } else {
-    navFixed.style.top = `${-navHeight}px`;
-  }
-});
+//   if (scrollValue > 450) {
+//     navFixed.style.top = `0px`;
+//   } else {
+//     navFixed.style.top = `${-navHeight}px`;
+//   }
+// });
