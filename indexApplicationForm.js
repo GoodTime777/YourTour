531ee6d057623779112eba4bbc2650442f3bbab1
Ольга <ots231088@gmail.com>
2024-11-18 17:20:11 +0300

    edits

--- indexApplicationForm.js
+++ indexApplicationForm.js
@@ -1,39 +1,62 @@
 const formApplication = document.forms.formApplication;
-const inputs = document.querySelectorAll(".input-application");
 const inputDateFrom = formApplication.elements.user_date_from;
 const inputDateTo = formApplication.elements.user_date_to;
+const inputName = formApplication.elements.user_name;
 const inputMail = formApplication.elements.user_email;
 const inputTel = formApplication.elements.user_tel;
+const age=formApplication.elements.age;
 
 const maskOptions = {
   mask: "+{7} ( 0 0 0 ) 0 0 0 - 0 0 - 0 0",
 };
 const mask = IMask(inputTel, maskOptions);
 
-inputTel.addEventListener("blur", () => {
+function validName(e) {
+  const name = /^([A-Za-zА-Яа-яЁёs]+\s)*[A-Za-zА-Яа-яЁёs]+$/;
+  return name.test(e);
+}
+
+function validEmail(e) {
+  const mail = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  return mail.test(e);
+}
+
+formApplication.addEventListener("submit", (e) => {
+  if (!validName(inputName.value) || inputName.value == 0) {
+    e.preventDefault();
+    inputName.style.color = "red";
+    inputName.focus();
+    inputName.addEventListener("change", () => {
+      inputName.style.color = "black";
+    });
+  } else if (validName(inputName.value)) {
+    inputName.style.color = "black";
+  }
+
   if (mask.typedValue.length < 11) {
+    e.preventDefault();
     inputTel.style.color = "red";
     inputTel.focus();
   } else if (mask.typedValue.length == 11) {
     inputTel.style.color = "black";
   }
-});
 
-inputMail.addEventListener("blur", () => {
-  inputMail.style.color = "black";
-  if (!inputMail.value.includes("@") || !inputMail.value.includes(".")) {
+  if (!validEmail(inputMail.value)) {
+    e.preventDefault();
     inputMail.style.color = "red";
     inputMail.focus();
   }
+
+  if(inputDateTo.value<inputDateFrom.value){   
+    e.preventDefault();
+    inputDateTo.style.color = "red";
+    inputDateTo.focus();
+  }
+
+  // age.forEach(btn=>{
+  //   console.log(btn.value)
+  // })
 });
 
 inputDateFrom.setAttribute("min", new Date().toISOString().split("T")[0]);
 inputDateTo.setAttribute("min", new Date().toISOString().split("T")[0]);
-
-//console.log(inputs)
-inputs.forEach(input => {
-  input.addEventListener('focus', () => {
-    input.style.boxShadow = " 0px 0px 0px 1px $gray_300 inset";
-    input.style.color = "$black";
-  })
-})
--- indexHeader.js
+++ indexHeader.js
@@ -1,22 +1,17 @@
-const header = document.querySelector(".header");
 const nav = document.querySelector(".header__navigation");
-const navFixed = document.querySelector(".navigation-scroll");
-const bd=document.getElementsByTagName('body');
-
 
 window.addEventListener("scroll", () => {
-  const scrll = window.scrollY;
+  const scroll = window.scrollY;
   console.log(window.scrollY);
-  if (scrll >= 450) {
+  if (scroll >= 450) {
     nav.classList.add("navigation-scroll");
-    //window.style.marginTop = 90 + "px";
-    //    navFixed.style.transform='translateY(0%)'
-    //  navFixed.style.transition='transform 1.5s'
-  }else {   
+    nav.style.top = "0px";
+   
+  } else if (scroll >= 300 && scroll < 450) {
+    nav.style.top = "-90px";
+   
+  } else {
+    nav.style.top = "0px";
     nav.classList.remove("navigation-scroll");
-   //nav.classList.add("navigation-scroll-hide");
-   //window.style.marginTop = 0 + "px";
-    // navFixed.style.transform='translateY(0%)'
-    // navFixed.style.transition='transform 1.5s'
   }
 });
