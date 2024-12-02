384dd85eb04c075c4f05df3b836d4ddb1178dd04
Ольга <ots231088@gmail.com>
2024-12-02 19:08:17 +0300

    edits

--- indexHeader.js
+++ indexHeader.js
@@ -2,14 +2,13 @@ const nav = document.querySelector(".header__navigation");
 
 window.addEventListener("scroll", () => {
   const scroll = window.scrollY;
-  console.log(window.scrollY);
+ // console.log(window.scrollY);
+ 
   if (scroll >= 450) {
     nav.classList.add("navigation-scroll");
     nav.style.top = "0px";
-   
   } else if (scroll >= 350 && scroll < 450) {
-    nav.style.top = "-93px";
-   
+    nav.style.top = "-90px";
   } else {
     nav.style.top = "0px";
     nav.classList.remove("navigation-scroll");
